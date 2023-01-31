ae413adb3041d2bb69ede76442f847bcba4cef70
DhruvS28 <45272029+DhruvS28@users.noreply.github.com>
2023-01-30 22:42:16 -0600

    A ton of minor changes
    Added backups to save previous copy
    Changed intro section (removed picture)
    Added new js file to store content data

---
+++ content.js
@@ -0,0 +1,104 @@
+
+
+var content_list = [];
+
+// var cl = content_list;
+
+
+content_list.push(
+	{
+		title: "JavaFX Aim Trainer",
+		date: "Dec 2022",
+		description: 
+			[ "GUI project using JavaFX and an MVC design structure.",
+				"Fetures: Multi-select moving/resizing/deleting, produces a statisitc distance-time chart"
+				// "Genre: Adventure, Mystery, Supernatural",
+			],
+		links: 
+			[
+				{
+					text: "GitHub Page",
+					url: "https://"
+				},
+				{
+					// text: "Web GL Build",
+					// url: "https://dhruvs28.github.io/ShadowSplit/"
+				}
+			],
+		image: "imgs/AimTrainer_poster.jpg"
+	}
+)
+
+content_list.push(
+	{
+		title: "ShadowSplit",
+		date: "Jun 2022",
+		description: 
+			[ "Unity game made, for a Game Jam, in a week working with a small team.",
+				"Contribution: Level design and level mechanics.",
+				"Genre: Adventure, Mystery, Supernatural"
+			],
+		links: 
+			[
+				{
+					text: "Itch.io Page",
+					url: "https://dhruvs28.itch.io/shadow-split"
+				},
+				{
+					text: "Web GL Build",
+					url: "https://dhruvs28.github.io/ShadowSplit/"
+				}
+			],
+		image: "imgs/ShadowSplit_poster.jpg"
+	}
+)
+
+content_list.push(
+	{
+		title: "Mousemarine",
+		date: "May 2022",
+		description:
+			[ "Unity game made, for a Game Jam, over a weekend working with a medium sized team.",
+				"Contribution: Most gameplay mechanics, including visual manipulation.",
+				"Genre: Platformer, Adventure, Comic"
+			],
+		links:
+			[
+				{
+					text: "Itch.io Page",
+					url: "https://goodvibes.itch.io/mousemarine"
+				},
+				{
+					text: "Web GL Build",
+					url: "https://dhruvs28.github.io/Mousemarine/"
+				}
+			],
+		image: "imgs/Mousemarine_poster.jpg"
+	}
+)
+
+content_list.push(
+	{
+		title: "MentalMatter",
+		date: "Jan 2022 - Apr 2022",
+		description:
+			[ "Mental excercising app that contains games to stimulate brain activity.",
+				"Contribution: Team managing lead, most features and mechanics, and UI/UX.",
+				"Genre: Memory, Vocabulary, Rhythm, Math"
+			],
+		links:
+			[
+				{
+					text: "Drive Link",
+					url: "https://drive.google.com/drive/folders/1cQzoSuJurMSceZZYIWW4bEWGZlLzCOgV?usp=sharing"
+				},
+				{
+					text: "Web GL Build",
+					url: "https://dhruvs28.github.io/MentalMatter/"
+				}
+			],
+		image: "imgs/MentalMatter_poster.jpg"
+	}
+)
+
+window.content_list = content_list;
